641a07f2f74e91578e033cb00dfb364523a0a489
sudhakar076020 <sudhakar7602m@gmail.com>
2025-08-08 09:46:16 +0530

    In the home component banner section added the motion for the social media icons and the cv button

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -20,7 +20,7 @@ const Home = () => {
         <motion.div
           initial={{ opacity: 0, y: 50 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8 }}
+          transition={{ duration: 1 }}
         >
           <div className="banner-content">
             <p className="greeting">Hello</p>
@@ -42,7 +42,7 @@ const Home = () => {
           <motion.div
             initial={{ opacity: 0, x: 50 }}
             animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.8 }}
+            transition={{ duration: 1.2 }}
           >
             <button type="button" className="get-my-cv-btn">
               GET MY CV
@@ -50,33 +50,75 @@ const Home = () => {
           </motion.div>
         </div>
 
-        <div className="banner-image">
-          <div className="get-my-cv-lg-container">
-            <div className="horizontal-lg-line"></div>
-            <button type="button" className="get-my-cv-btn">
-              GET MY CV
-            </button>
-          </div>
+        <motion.div
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.2 }}
+        >
+          <div className="banner-image">
+            <motion.div
+              initial={{ opacity: 0, x: -50 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.8, delay: 1.10 }}
+            >
+              <div className="get-my-cv-lg-container">
+                <div className="horizontal-lg-line"></div>
+                <button type="button" className="get-my-cv-btn">
+                  GET MY CV
+                </button>
+              </div>
+            </motion.div>
 
-          <div className="banner-social-icons">
-            <a href="" target="_blank" rel="noopener noreferrer">
-              <FaGithub className="social-icon" />
-            </a>
-            <a href="" target="_blank" rel="noopener noreferrer">
-              <FaLinkedin className="social-icon" />
-            </a>
-            <a href="" target="_blank" rel="noopener noreferrer">
-              <FaTwitter className="social-icon" />
-            </a>
-            <a href="" target="_blank" rel="noopener noreferrer">
-              <FaFacebookSquare className="social-icon" />
-            </a>
-            <div className="vertical-line"></div>
-            <button type="button" className="circle-btn">
-              <GoTriangleDown className="down-arrow-icon" />
-            </button>
+            <div className="banner-social-icons">
+              <motion.div
+                initial={{ opacity: 0, y: -50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.8, delay: 1 }}
+              >
+                <a href="" target="_blank" rel="noopener noreferrer">
+                  <FaGithub className="social-icon" />
+                </a>
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0, y: -50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.8, delay: 1.2 }}
+              >
+                <a href="" target="_blank" rel="noopener noreferrer">
+                  <FaLinkedin className="social-icon" />
+                </a>
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0, y: -50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.8, delay: 1.4 }}
+              >
+                <a href="" target="_blank" rel="noopener noreferrer">
+                  <FaTwitter className="social-icon" />
+                </a>
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0, y: -50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.8, delay: 1.6 }}
+              >
+                <a href="" target="_blank" rel="noopener noreferrer">
+                  <FaFacebookSquare className="social-icon" />
+                </a>
+                <div className="vertical-line"></div>
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0, y: -50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.8, delay: 1.8 }}
+              >
+                <button type="button" className="circle-btn">
+                  <GoTriangleDown className="down-arrow-icon" />
+                </button>
+              </motion.div>
+            </div>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
